1e585a5148b8ca6da2a77a94f767304aebf93c73
EsmondKim <ekelectronicmail@gmail.com>
2021-02-10 22:06:54 -0600

    Experimenting with UPDATE routes.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,6 +5,7 @@ router.get("/api/workouts", (req, res) => {
   Exercises.find({})
     .sort({ date: -1 })
     .then((dbExercises) => {
+      console.log(dbExercises);
       res.json(dbExercises);
     })
     .catch((err) => {
@@ -21,6 +22,34 @@ router.post("/api/workouts/", ({ body }, res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
+});
+
+router.post("/api/workouts/:params", (req, res) => {
+  console.log(req);
+  console.log(params);
+  db.exercises.update(
+    {
+      _id: mongojs.ObjectId(req.params.id),
+    },
+    {
+      $set: {
+        type: req.body.type,
+        name: req.body.name,
+        duration: req.body.duration,
+        weight: req.body.weight,
+        reps: req.body.reps,
+        sets: req.body.sets,
+        modified: Date.now(),
+      },
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
 });
 
 module.exports = router;
