bd1dad235dafc434689c8bb9c2e0f4e3d9b43013
EsmondKim <ekelectronicmail@gmail.com>
2021-02-18 00:43:43 -0600

    commented out api.js line 11 because it's 400 erroring Heroku.

--- public/api.js
+++ public/api.js
@@ -8,7 +8,7 @@ const API = {
     }
     const json = await res.json();
 
-    return json[json.length - 1];
+    //return json[json.length - 1];
   },
   async addExercise(data) {
     const id = location.search.split("=")[1];
